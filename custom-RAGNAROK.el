5f49bd2342636ba74d766fbc9a4575ebe1222a95
Chap Lovejoy <chaplovejoy@gmail.com>
2016-09-18 15:30:18 -0400

    Update customize vars

--- custom-RAGNAROK.el
+++ custom-RAGNAROK.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default-frame-alist (quote ((width . 110) (height . 55)))))
+ '(default-frame-alist (quote ((width . 110) (height . 55))))
+ '(package-selected-packages
+   (quote
+    (rspec-mode rbenv crux counsel-projectile yaml-mode which-key wgrep web-mode use-package try smex scss-mode sass-mode robe projectile-rails persp-projectile multiple-cursors magit leuven-theme git-gutter fsharp-mode flymake-sass flymake-ruby flymake-css flymake-coffee expand-region counsel color-theme-sanityinc-tomorrow coffee-mode avy ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
